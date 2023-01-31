66efd72b016be475bc50d64b611245f91ed54381
Rushikesh Deshmukh <rushikesh.deshmukh2@ing.com>
2023-01-31 22:59:53 +0530

    2.03

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header>
-            Lerna is the original monorepo tool! lets see 2.02
+            Lerna is the original monorepo tool! lets see 2.03
         </header>
     );
 }
