9ba77a33d2439099e83a6dbb96c008ea4a64787f
Rushikesh Deshmukh <rushikesh.deshmukh2@ing.com>
2023-01-31 16:22:27 +0530

    added path

--- packages/remixapp/app/routes/index.tsx
+++ packages/remixapp/app/routes/index.tsx
@@ -1,5 +1,5 @@
-import {Header} from 'header';
-import {Footer} from 'footer';
+import {Header} from '../../../header/src/Header.js';
+import {Footer} from '../../../footer/src/Footer.js';
 
 export default function Index() {
     return (
