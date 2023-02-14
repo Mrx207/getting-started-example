cc32c74def06109965ebd4e3db8edca50222b587
Rushikesh Deshmukh <rushikesh.deshmukh2@ing.com>
2023-02-14 16:00:38 +0530

    feat:change in header

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Header() {
     return (
         <header>
-            Lerna is the original monorepo tool! lets see 2.04
+            Lerna is the original monorepo tool! lets see 2.05
         </header>
     );
 }
